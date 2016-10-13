70922ddd7fb1029bf8c28637325d4b12db266107
MilosDLo <milosd_1@hotmail.com>
2016-10-13 10:59:30 +0200

    Added SQLite connection and database,reading and writing to database,removed unused threads

---
+++ TestWinService/JsonClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace TestWinService
+{
+    class JsonClass
+    {
+        public int IDJson { get; set; }
+        public string Desc { get; set; }
+        public string  Config { get; set; }
+
+
+    }
+}
--- TestWinService/Service1.cs
+++ TestWinService/Service1.cs
@@ -10,11 +10,18 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Net.Http;
 
 namespace TestWinService
 {
     public partial class Service1 : ServiceBase
     {
+
+        string connString = @"Data Source=c:\mydb.sqlite;Version=3";
+        JsonClass jc;
+
+
         public Service1()
         {
             InitializeComponent();
@@ -24,11 +31,10 @@ namespace TestWinService
             }
             eventLog1.Source = "MySource";
             eventLog1.Log = "MyNewLog";
+            jc = null;
+
+
         }
-
-
-        PersonJson person = null;
-
         string jsonString = @"{
                 'Name' : 'Milos',
                 'Adress' : 'Bg'                        
@@ -38,58 +44,121 @@ namespace TestWinService
 
         protected override void OnStart(string[] args)
         {
-            // Library.WriteToLog("\n\n" + DateTime.Now.ToString()+": Servis pokrenut");
+            
             eventLog1.WriteEntry("Servis pokrenut");
 
-            #region ako se dobije url sa json-om
-             var jsonUrl = "http://echo.jsontest.com/Adress/Bg/Name/Milos";
-            string jsonStringUrl = "";
-             using (WebClient wc = new WebClient())
-             {
-                 jsonStringUrl = wc.DownloadString(jsonUrl);
-             }
-            /*
-             //ili asihrono ako je .NET 4.5 ili veci
-             using (var httpClient = new HttpClient()){
-              var json = await httpClient.GetStringAsync("url");
-              }
-              */
-            #endregion
-
-            //sad parsiramo sa JSON.NET
-            //ako je nekog atributa nema bice null
-            //person = JsonConvert.DeserializeObject<PersonJson>(jsonString);
-            person = JsonConvert.DeserializeObject<PersonJson>(jsonStringUrl);
-
-            Thread.CurrentThread.Name = "main";
-            PrintThread();
-            
-
-            Thread t1 = new Thread(PrintThread);
-            t1.Name = "1.";
-            t1.Start();
-            //t1.Join();
-            
-            Thread t2 = new Thread(PrintThread);
-            t2.Name = "2.";
-            t2.Start();
-            
-            Thread t3 = new Thread(PrintThread);
-            t3.Name = "3.";
-            t3.Start();
-
-
-
+     
+            Thread t1 = new Thread(WaitingForJson);
+            t1.Name = "ThreadWaitingForJson";
+            t1.IsBackground = true;
+            t1.Start();               
 
             eventLog1.WriteEntry("Kraj main thread-a");
         }
 
+
+        private void WaitingForJson()
+        {
+            while (true)
+            {
+                #region ako se dobije url sa json-om
+                var jsonUrl = "http://echo.jsontest.com/Config/b-12-4/Desc/Iron/idjson/1212";
+                string jsonStringUrl = "";
+                using (WebClient wc = new WebClient())
+                {
+                    jsonStringUrl = wc.DownloadString(jsonUrl);
+                }
+                /*
+                 //ili asihrono ako je .NET 4.5 ili veci
+                 using (var httpClient = new HttpClient()){
+                  var json = await httpClient.GetStringAsync("url");
+                  }
+                  */
+                #endregion
+
+
+                using (SQLiteConnection conn = new SQLiteConnection(connString))
+                {
+                    conn.Open();
+
+
+                    #region kreiranje baze i tabele
+                    //     using (SQLiteCommand command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS [JsonTable] (id INTEGER PRIMARY KEY AUTOINCREMENT,idjson INTEGER,desc TEXT,config TEXT)", conn))
+                    //     {
+                    //         int i = command.ExecuteNonQuery();
+                    //         eventLog1.WriteEntry(i.ToString());  
+                    //     }
+                    //
+                    //     using (SQLiteCommand command = new SQLiteCommand("INSERT INTO JsonTable (idjson,desc,config) values (112,'proba','nestonesto') ", conn))
+                    //     {
+                    //         int i = command.ExecuteNonQuery();
+                    //         eventLog1.WriteEntry(i.ToString());
+                    //     }
+                    #endregion
+
+                    jc = JsonConvert.DeserializeObject<JsonClass>(jsonStringUrl);
+
+
+                    using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM JsonTable ORDER BY id DESC LIMIT 1", conn))
+                    {
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                if (jc.IDJson == Int32.Parse(reader[1].ToString()))
+                                {
+                                    eventLog1.WriteEntry("Isti jsoni");
+                                }
+                                else
+                                {
+                                    string commandText = String.Format("INSERT INTO JsonTable (idjson,desc,config) values ({0},'{1}','{2}')", jc.IDJson, jc.Desc, jc.Config);
+                     
+                                    using (SQLiteCommand commandJsonWrite = new SQLiteCommand(commandText, conn))
+                                    {
+                                        commandJsonWrite.ExecuteNonQuery();
+                                        eventLog1.WriteEntry("Ubacio u bazu");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    // PrintThread();
+                }
+                eventLog1.WriteEntry("Proveravamo opet za 60sec.");
+                Thread.Sleep(60000);
+            }
+
+            //  Thread t1 = new Thread(PrintThread);
+            //  t1.Name = "1.";
+            //  t1.Start();
+            //  //t1.Join();
+            //  
+            //  Thread t2 = new Thread(PrintThread);
+            //  t2.Name = "2.";
+            //  t2.Start();
+
+
+            //fora pokupiti body sa urla
+            //   var task = MakeRequest();
+            //   task.Wait();
+            //
+            //   var response = task.Result;
+            //   var body = response.Content.ReadAsStringAsync().Result;
+            //   eventLog1.WriteEntry(body);
+        }
+
+
+        // private static async Task<HttpResponseMessage> MakeRequest()
+        // {
+        //     var httpClient = new HttpClient();
+        //     return await httpClient.GetAsync(new Uri("http://requestb.in/1f19ydi1"));
+        // }
+
         private void PrintThread()
         {
-            var message = "Ispisujem iz "  +Thread.CurrentThread.Name+ " thread-a: " +person.Name+", "+person.Adress;
+            var message = "Ispisujem iz "  +Thread.CurrentThread.Name+ " thread-a: " + jc.IDJson +", " +jc.Desc+", "+jc.Config;
             for (int i = 0; i < 3; i++)
             {
-                // Library.WriteToLog(message);
                 eventLog1.WriteEntry(message);
                 Thread.Sleep(1000);
             }
